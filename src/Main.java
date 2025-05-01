ae8c70ff14bbd21a30ee3557bf906a9f438bf687
JeanPeralta <jean.peralta@udla.edu.ec>
2025-05-01 17:00:52 -0500

    dd

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Crear perro
+        System.out.print("Informacion de la especie: ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Raza del perro: ");
+        String raza = scanner.nextLine();
+
+        System.out.print("Edad del perro (en años): ");
+        int edad = scanner.nextInt();
+
+        Mascotas masc = new Mascotas(nombre, raza, edad);
+
+
+        System.out.println("\n¿Qué deseas hacer con " + nombre + "?");
+        System.out.println("1. Informacion de la especie");
+        System.out.println("2. Vender, maximo 50 unidades para vender");
+        System.out.println("3. Comprar mascota minimo 5 unidades");
+        System.out.println("4.Buscar especie");
+        System.out.println("5. Especie menos vendida");
+
+
+
+
+
+
+
+
         }
     }
-}
--- src/Mascotas.java
+++ src/Mascotas.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+
 
 public class Mascotas {
+    String Nombre;
+    int Vida_promedio;
+    String Clase_animal;
+    int precio;
+    int Cantidad_actual;
+    int Cantidad_total;
+
+    venta_max= 50;
+
+
+    public Mascotas(String Nombre, int Vida_promedio, String Clase_animal,int precio,  int Cantidad_actual, int Cantidad_total ) {
+
+        this.Nombre = Nombre;
+        this.Vida_promedio = Vida_promedio;
+        this.Clase_animal = Clase_animal;
+        this.precio = precio;
+        this.Cantidad_actual = Cantidad_actual;
+        this.Cantidad_total = Cantidad_total;
+
+    }
+    public void ladrar() {
+        System.out.println(Nombre + " dice: ¡Guau guau!");
+
+    }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
