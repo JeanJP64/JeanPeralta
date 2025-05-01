41a18dbc4e46989d0a677380ac600c612ca43eff
Jean <jean.peralta@udla.edu.ec>
2025-05-01 17:26:46 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 
 public class Main {
@@ -16,17 +14,43 @@ public class Main {
         System.out.print("Edad del perro (en años): ");
         int edad = scanner.nextInt();
 
-        Mascotas masc = new Mascotas(nombre, raza, edad);
+        Mascotas masc = new Mascotas(Nombre, Vida_promedio, Clase_animal. precio, Cantidad_actual,Cantidad_total );
+        int opcion;
+        do{
+
+            System.out.println("\n¿Qué deseas hacer con " + nombre + "?");
+            System.out.println("1. Informacion de la especie");
+            System.out.println("2. Vender, maximo 50 unidades para vender");
+            System.out.println("3. Comprar mascota minimo 5 unidades");
+            System.out.println("4.Buscar especie");
+            System.out.println("5. Especie menos vendida");
 
 
-        System.out.println("\n¿Qué deseas hacer con " + nombre + "?");
-        System.out.println("1. Informacion de la especie");
-        System.out.println("2. Vender, maximo 50 unidades para vender");
-        System.out.println("3. Comprar mascota minimo 5 unidades");
-        System.out.println("4.Buscar especie");
-        System.out.println("5. Especie menos vendida");
+        opcion = scanner.nextInt();
 
+                    switch (opcion) {
+                        case 1:
+                            miPerro.ladrar();
+                            break;
+                        case 2:
+                            miPerro.vacunar();
+                            break;
+                        case 3:
+                            miPerro.perderVacuna();
+                            break;
+                        case 4:
+                            miPerro.mostrarInfo();
+                            break;
+                        case 0:
+                            System.out.println("Saliendo...");
+                            break;
+                        default:
+                            System.out.println("Opción no válida.");
+                    }
 
+                } while (opcion != 0);
+
+                scanner.close();
 
 
 
